3557e0004003739ef3ab30da4e4e13b387855d09
Luca Magnoni <luca.magnoni@cnaf.infn.it>
2008-12-01 16:19:54 +0000

    SQL scripts for database creation have to be distributed together with the BackEnd component

---
+++ etc/db/storm_mysql_update_from_1.3.2000_to_1.4.sql
@@ -0,0 +1,11 @@
+--- 
+--- Update StoRM database from 1.3.19 to 1.3.20.
+--- Please not that the version of database in the 1.3.19 relase  is 1.0.0
+---
+
+USE storm_db;
+
+REPLACE INTO db_version (major,minor,revision,description) VALUES (1,4,00,'1 Dec 2008');
+
+
+
