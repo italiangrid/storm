a235bc5cbbfa93d18d747551eab9f4e22b5982d5
Riccardo Zappi <riccardo.zappi@cnaf.infn.it>
2009-08-06 15:48:06 +0000

    work in progress (RecallTable) commit

--- src/it/grid/storm/tape/recalltable/RecallTableService.java
+++ src/it/grid/storm/tape/recalltable/RecallTableService.java
@@ -7,14 +7,18 @@ import it.grid.storm.config.Configuration;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.ws.rs.core.UriBuilder;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.sun.grizzly.http.SelectorThread;
 import com.sun.grizzly.http.embed.GrizzlyWebServer;
 import com.sun.grizzly.http.servlet.ServletAdapter;
+import com.sun.jersey.api.container.grizzly.GrizzlyWebContainerFactory;
 import com.sun.jersey.api.core.PackagesResourceConfig;
 
 /**
@@ -25,6 +29,8 @@ public class RecallTableService {
 
     private static final Logger log = LoggerFactory.getLogger(RecallTableService.class);
     private static Configuration config = Configuration.getInstance();
+    private static int serverPort = 9998;
+    private static SelectorThread httpThreadSelector;
 
     public static final URI BASE_URI = RecallTableService.getBaseURI();
 
@@ -38,15 +44,44 @@ public class RecallTableService {
         return UriBuilder.fromUri("http://localhost/").port(RecallTableService.getPort()).build();
     }
 
-    public static void start() throws IOException {
+    
+    protected static void initServer() {
+        serverPort = RecallTableService.getPort();
+
+    }
+    
+    
+    protected static SelectorThread startServer() throws IOException {
+        final Map<String, String> initParams = new HashMap<String, String>();
+        initParams.put(PackagesResourceConfig.PROPERTY_PACKAGES, "it.grid.storm.tape.recalltable.resources");
+
+        System.out.println("Starting grizzly...");
+        SelectorThread threadSelector = GrizzlyWebContainerFactory.create(BASE_URI, initParams);
+        return threadSelector;
+    }
+
+    
+    public static void startWithAdapter() throws IOException {
         GrizzlyWebServer ws = new GrizzlyWebServer(RecallTableService.getPort());
         ServletAdapter jerseyAdapter = new ServletAdapter();
         jerseyAdapter.setServletInstance(new com.sun.jersey.spi.container.servlet.ServletContainer());
+
         jerseyAdapter.addInitParameter(PackagesResourceConfig.PROPERTY_PACKAGES,
                 "it.grid.storm.tape.recalltable.resources");
+        System.out.println("Property packages : " + PackagesResourceConfig.PROPERTY_PACKAGES);
         ws.addGrizzlyAdapter(jerseyAdapter, new String[] { "/" });
         ws.getSelectorThread().enableMonitoring();
         ws.start();
     } 
+    
+    public static void start() throws IOException {
+        initServer();
+        httpThreadSelector = startServer();
+
+    }
+
+    public static void stop() throws IOException {
+        httpThreadSelector.stopEndpoint();
+    }
     
 }
