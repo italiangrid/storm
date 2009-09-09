8dd0b5cfb5a3d2954eb0b02999ab2f7bd7f68d5d
Alberto Forti <alberto.forti@cnaf.infn.it>
2009-09-09 07:55:30 +0000

    fix

--- src/it/grid/storm/namespace/model/StorageClassType.java
+++ src/it/grid/storm/namespace/model/StorageClassType.java
@@ -1,45 +1,21 @@
 package it.grid.storm.namespace.model;
 
-public class StorageClassType {
+public enum StorageClassType {
 
-    public final static StorageClassType T0D0 = new StorageClassType("T0D0", "T0D0");
-    public final static StorageClassType T0D1 = new StorageClassType("T0D1", "T0D1");
-    public final static StorageClassType T1D0 = new StorageClassType("T1D0", "T1D0");
-    public final static StorageClassType T1D1 = new StorageClassType("T1D1", "T1D1");
-    public final static StorageClassType UNKNOWN = new StorageClassType("UNKNOWN",
-                                                                        "Storage Class Type UNKNOWN!");
+    T0D0("T0D0", "T0D0"),
+    T0D1("T0D1", "T0D1"),
+    T1D0("T1D0", "T1D0"),
+    T1D1("T1D1", "T1D1"),
+    UNKNOWN("UNKNOWN", "Storage Class Type UNKNOWN!");
+    
     private String storageClassTypeString;
     private String stringSchema;
-    private StorageClassType storageClassType;
-    private boolean tapeEnabled = false;
 
-    public StorageClassType(String storageClassTypeString, String stringSchema) {
+    private StorageClassType(String storageClassTypeString, String stringSchema) {
 
         this.storageClassTypeString = storageClassTypeString;
         this.stringSchema = stringSchema;
-
-        if (T0D0.getStorageClassTypeString().equals(storageClassTypeString)) {
-
-            storageClassType = StorageClassType.T0D0;
-
-        } else if (T0D1.getStorageClassTypeString().equals(storageClassTypeString)) {
-
-            storageClassType = StorageClassType.T0D1;
-
-        } else if (T1D0.getStorageClassTypeString().equals(storageClassTypeString)) {
-
-            storageClassType = StorageClassType.T1D0;
-            tapeEnabled = true;
-
-        } else if (T1D1.getStorageClassTypeString().equals(storageClassTypeString)) {
-
-            storageClassType = StorageClassType.T1D1;
-            tapeEnabled = true;
-
-        } else {
-
-            storageClassType = StorageClassType.UNKNOWN;
-        }
+        
     }
 
     /**
@@ -48,33 +24,14 @@ public class StorageClassType {
      * @return StorageClassType
      */
     public static StorageClassType getStorageClassType(String storageClassTypeString) {
-
-        if (storageClassTypeString.equals(T0D0.toString())) {
-            return StorageClassType.T0D0;
-        }
         
-        if (storageClassTypeString.equals(T0D1.toString())) {
-            return StorageClassType.T0D1;
-        }
-        
-        if (storageClassTypeString.equals(T1D0.toString())) {
-            return StorageClassType.T1D0;
-        }
-        
-        if (storageClassTypeString.equals(T1D1.toString())) {
-            return StorageClassType.T1D1;
+        for (StorageClassType sct : StorageClassType.values()) {
+            if (sct.getStorageClassTypeString().equals(storageClassTypeString)) {
+                return sct;
+            }
         }
 
-        return StorageClassType.UNKNOWN;
-    }
-
-    /**
-     * Returns the storage class type identified by this instance.
-     * 
-     * @return the storage class type.
-     */
-    public StorageClassType getStorageClassType() {
-        return storageClassType;
+        return UNKNOWN;
     }
 
     /**
@@ -91,12 +48,16 @@ public class StorageClassType {
     }
 
     public boolean isTapeEnabled() {
-        return tapeEnabled;
+        
+        if (this.equals(T1D0) || this.equals(T1D1)) {
+            return true;
+        }
+        
+        return false;
     }
 
     //Only get method for Schema
     public String toString() {
         return this.stringSchema;
     }
-
 }
