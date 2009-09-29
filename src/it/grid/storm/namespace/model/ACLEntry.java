98059196249fe51d9fce97742f5d734d088b3520
Riccardo Zappi <riccardo.zappi@cnaf.infn.it>
2009-09-29 16:51:06 +0000

    New mechanism to retrieve GroupId ( bug defaultACL )

--- src/it/grid/storm/namespace/model/ACLEntry.java
+++ src/it/grid/storm/namespace/model/ACLEntry.java
@@ -1,8 +1,8 @@
 package it.grid.storm.namespace.model;
 
 import it.grid.storm.filesystem.FilesystemPermission;
+import it.grid.storm.namespace.util.userinfo.EtcGroupReader;
 import it.grid.storm.namespace.util.userinfo.UserInfoException;
-import it.grid.storm.namespace.util.userinfo.UserInfoExecutor;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,32 +11,37 @@ public class ACLEntry {
 
     private static final Logger LOG = LoggerFactory.getLogger(ACLEntry.class);
 
-    private String groupName;
+    private final String groupName;
     private int groupId = -1;
-    private FilePermissionType permission;
+    private final FilePermissionType permission;
 
     public ACLEntry(String groupName, String permissionString) throws PermissionException {
 
-        //Digest the permission
-        this.permission = FilePermissionType.getFilePermissionType(permissionString);
+        // Digest the permission
+        permission = FilePermissionType.getFilePermissionType(permissionString);
         if (permission.equals(FilePermissionType.UNKNOWN)) {
-            LOG.error("Unble to understand permission '"+permissionString+"'");
-            throw new PermissionException("Unble to understand permission '"+permissionString+"'");
+            LOG.error("Unble to understand permission '" + permissionString + "'");
+            throw new PermissionException("Unble to understand permission '" + permissionString + "'");
         }
 
-        //Digest the GroupName and Retrieve the GroupId
+        // Digest the GroupName and Retrieve the GroupId
         this.groupName = groupName;
         try {
-            this.groupId = UserInfoExecutor.retrieveGroupID(groupName);
-            //this.groupId = UserInfoExecutor.retrieveGroupID_ENT(groupName);
-        }
-        catch (UserInfoException ex) {
-            LOG.error("ACL Entry: ('"+groupName+"') --> "+ex);
+            boolean isDefined = EtcGroupReader.isGroupDefined(groupName);
+            if (!isDefined) {
+                LOG.error("The groupName '" + groupName + "' does not exist!");
+            } else {
+                LOG.debug("Checking if groupName '" + groupName + "' is defined: " + isDefined);
+            }
+            groupId = EtcGroupReader.getGroupId(groupName);
+            LOG.debug("GroupID of '" + groupName + "' = " + groupId);
+            // this.groupId = UserInfoExecutor.retrieveGroupID(groupName);
+            // this.groupId = UserInfoExecutor.retrieveGroupID_ENT(groupName);
+        } catch (UserInfoException ex) {
+            LOG.error("ACL Entry: ('" + groupName + "') --> " + ex);
             throw new PermissionException(ex.getMessage());
         }
     }
-    
-    
 
     public int getGroupID() {
         return groupId;
@@ -48,23 +53,24 @@ public class ACLEntry {
 
     public FilesystemPermission getFilesystemPermission() {
         switch (permission.getOrdinalNumber()) {
-        //READ 0 : FilePermissionType.READ.getOrdinalNumber()
-        case 0 : return FilesystemPermission.Read;
-        //READWRITE 1 : FilePermissionType.READWRITE.getOrdinalNumber()
-        case 1 : return FilesystemPermission.ReadWrite;
-        //WRITE 2 : FilePermissionType.WRITE.getOrdinalNumber()
-        case 2 : return FilesystemPermission.Write;
-        //DEFAULT VALUE (is it possible this case?) == READ
-        default: return FilesystemPermission.Read;
+            // READ 0 : FilePermissionType.READ.getOrdinalNumber()
+            case 0:
+                return FilesystemPermission.Read;
+                // READWRITE 1 : FilePermissionType.READWRITE.getOrdinalNumber()
+            case 1:
+                return FilesystemPermission.ReadWrite;
+                // WRITE 2 : FilePermissionType.WRITE.getOrdinalNumber()
+            case 2:
+                return FilesystemPermission.Write;
+                // DEFAULT VALUE (is it possible this case?) == READ
+            default:
+                return FilesystemPermission.Read;
         }
     }
 
-
     @Override
     public String toString() {
-        return "group: "+this.groupId+" ("+this.groupName+")" + " permission: "+this.permission;
+        return "group: " + groupId + " (" + groupName + ")" + " permission: " + permission;
     }
-
-
 
 }
---
+++ src/it/grid/storm/namespace/util/userinfo/EtcGroupReader.java
@@ -0,0 +1,117 @@
+/**
+ * 
+ */
+package it.grid.storm.namespace.util.userinfo;
+
+import it.grid.storm.config.Configuration;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author ritz
+ */
+public class EtcGroupReader {
+
+    private static final String etcGroupLinuxFN = "/etc/group";
+    private static final String etcGroupTest = "etc-group";
+
+    private static final Logger log = LoggerFactory.getLogger(EtcGroupReader.class);
+    private HashSet<String> groups = null;
+    private HashMap<String, Integer> groupId = null;
+    private static final EtcGroupReader instanceLinux = new EtcGroupReader();
+    private EtcGroupReader instanceTest;
+
+    private EtcGroupReader() {
+        init(etcGroupLinuxFN);
+    }
+
+    private EtcGroupReader(String filename) {
+        init(filename);
+    }
+
+    private void init(String filename) {
+        groups = new HashSet<String>();
+        groupId = new HashMap<String, Integer>();
+        try {
+            BufferedReader in = new BufferedReader(new FileReader("/etc/group"));
+            String str;
+            while ((str = in.readLine()) != null) {
+                // Parsing the line
+                String patternStr = ":";
+                String[] fields = str.split(patternStr);
+                String groupName = fields[0];
+                groups.add(groupName);
+                int gid = Integer.parseInt(fields[2]);
+                Integer gId = new Integer(gid);
+                groupId.put(groupName, gId);
+            }
+            in.close();
+        } catch (IOException e) {
+            log.error("Unable to read the '" + filename + "' file." + e);
+        }
+    }
+
+    private static EtcGroupReader getInstance(boolean test) {
+        if (test) {
+            String configurationDir = Configuration.getInstance().getConfigurationDir();
+            String etcGroupFN = configurationDir + File.pathSeparator + etcGroupTest;
+            log.debug("TEST etc-group filename = " + etcGroupFN);
+            return new EtcGroupReader(etcGroupFN);
+        }
+        return instanceLinux;
+    }
+
+    public static boolean isGroupDefined(String groupName) {
+        boolean result = false;
+        EtcGroupReader gr = getInstance(false);
+        result = gr.groups.contains(groupName);
+        if (!result) {
+            // Try a refresh
+            gr.init(etcGroupLinuxFN);
+            result = gr.groups.contains(groupName);
+        }
+        return result;
+    }
+
+    public static boolean isGroupDefined(String groupName, boolean test) {
+        boolean result = false;
+        EtcGroupReader gr = getInstance(test);
+        result = gr.groups.contains(groupName);
+        if (!result) {
+            // Try a refresh
+            gr.init(etcGroupTest);
+            result = gr.groups.contains(groupName);
+        }
+        return result;
+    }
+
+    public static int getGroupId(String groupName) {
+        int result = 33333;
+        if (isGroupDefined(groupName)) {
+            EtcGroupReader gr = getInstance(false);
+            result = gr.groupId.get(groupName);
+        } else {
+            log.error("Unable to retrieve groupId of groupName '" + groupName + "'");
+        }
+        return result;
+    }
+
+    public static int getGroupId(String groupName, boolean test) {
+        int result = 33333;
+        if (isGroupDefined(groupName)) {
+            EtcGroupReader gr = getInstance(test);
+            result = gr.groupId.get(groupName);
+        } else {
+            log.error("Unable to retrieve groupId of groupName '" + groupName + "'");
+        }
+        return result;
+    }
+}
