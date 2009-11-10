5edede8af3f5d1164ac13bca72856d1fe8b8dda1
Riccardo Zappi <riccardo.zappi@cnaf.infn.it>
2009-11-10 16:52:29 +0000

    Removed StorageFile from persistence.

--- src/it/grid/storm/persistence/model/StorageSpaceTO.java
+++ src/it/grid/storm/persistence/model/StorageSpaceTO.java
@@ -11,7 +11,6 @@
  */
 package it.grid.storm.persistence.model;
 
-
 import it.grid.storm.common.types.VO;
 import it.grid.storm.config.DefaultValue;
 import it.grid.storm.griduser.GridUserInterface;
@@ -26,22 +25,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- *
  * @author Riccardo Zappi - riccardo.zappi AT cnaf.infn.it
  * @version $Id: StorageSpaceTO.java,v 1.13 2006/06/29 14:46:25 aforti Exp $
  */
 public class StorageSpaceTO implements Serializable, Comparable {
-
 
     /**
      *Logger.
      */
     private static final Logger log = LoggerFactory.getLogger(StorageSpaceTO.class);
 
-
-    //----- PRIMARY KEY ----//
-    private Long storageSpaceId = null; //Persistence Object IDentifier
-    //----- FIELDS ----//
+    // ----- PRIMARY KEY ----//
+    private Long storageSpaceId = null; // Persistence Object IDentifier
+    // ----- FIELDS ----//
     private String ownerName = null;
     private String voName = null;
     private String spaceType = null;
@@ -55,102 +51,97 @@ public class StorageSpaceTO implements Serializable, Comparable {
     private long lifetime = -1L;
     private String storageInfo = null;
 
-    //----- ASSOCIATIONS ----//
-    //Relationship with StorageFile [one-to-many (not-null = false)]
-    private Set storageFiles = new HashSet();
-    //Component splitted into ownerName e voName
-    private GridUserInterface owner = null; //The maker
+    // ----- ASSOCIATIONS ----//
+    // Relationship with StorageFile [one-to-many (not-null = false)]
+    private final Set storageFiles = new HashSet();
+    // Component splitted into ownerName e voName
+    private GridUserInterface owner = null; // The maker
+
     // ********************** Constructor methods ********************** //
 
     /**
      * No-arg constructor for JavaBean tools.
      */
-    public StorageSpaceTO()
-    {
+    public StorageSpaceTO() {
         super();
     }
 
     /**
      * Minimal constructor.
-     *
+     * 
      * @param maker User
      */
-    public StorageSpaceTO(GridUserInterface maker)
-    {
-        //Always exists a creator!
-        this.owner = maker;
-        this.ownerName = maker.getDn();
-        this.voName = getVOName(maker);
+    public StorageSpaceTO(GridUserInterface maker) {
+        // Always exists a creator!
+        owner = maker;
+        ownerName = maker.getDn();
+        voName = getVOName(maker);
 
-        //No Alias (or Token description) is setted
-        this.alias = null;
+        // No Alias (or Token description) is setted
+        alias = null;
 
         /**
          * The below parameters are filled with DEFAULT values for named user.
          */
-        this.spaceType = DefaultValue.getNamedVO_SpaceType(voName);;
-        this.guaranteedSize = DefaultValue.getNamedVO_GuaranteedSpaceSize(voName);
-        this.totalSize = DefaultValue.getNamedVO_TotalSpaceSize(voName);
-        this.lifetime = DefaultValue.getNamedVO_SpaceLifeTime(voName);
+        spaceType = DefaultValue.getNamedVO_SpaceType(voName);;
+        guaranteedSize = DefaultValue.getNamedVO_GuaranteedSpaceSize(voName);
+        totalSize = DefaultValue.getNamedVO_TotalSpaceSize(voName);
+        lifetime = DefaultValue.getNamedVO_SpaceLifeTime(voName);
 
         /**
-         *  The below parameters are filled with GENERATED values for named user.
+         * The below parameters are filled with GENERATED values for named user.
          */
-        //StoRM generates a space_token
-        this.spaceToken = (new GUID()).toString();
-        this.spaceFile = "sf-"+spaceToken;
+        // StoRM generates a space_token
+        spaceToken = (new GUID()).toString();
+        spaceFile = "sf-" + spaceToken;
     }
 
-    public StorageSpaceTO(GridUserInterface maker, String alias)
-    {
-        //Always exists a creator!
-        this.owner = maker;
-        this.ownerName = maker.getDn();
-        this.voName = getVOName(maker);
-        //No Alias (or Token description) is setted
+    public StorageSpaceTO(GridUserInterface maker, String alias) {
+        // Always exists a creator!
+        owner = maker;
+        ownerName = maker.getDn();
+        voName = getVOName(maker);
+        // No Alias (or Token description) is setted
         this.alias = alias;
 
         /**
          * The below parameters are filled with DEFAULT values for named user.
          */
-        this.spaceType = DefaultValue.getNamedVO_SpaceType(voName);;
-        this.guaranteedSize = DefaultValue.getNamedVO_GuaranteedSpaceSize(voName);
-        this.totalSize = DefaultValue.getNamedVO_TotalSpaceSize(voName);
-        this.lifetime = DefaultValue.getNamedVO_SpaceLifeTime(voName);
+        spaceType = DefaultValue.getNamedVO_SpaceType(voName);;
+        guaranteedSize = DefaultValue.getNamedVO_GuaranteedSpaceSize(voName);
+        totalSize = DefaultValue.getNamedVO_TotalSpaceSize(voName);
+        lifetime = DefaultValue.getNamedVO_SpaceLifeTime(voName);
 
         /**
-         *  The below parameters are filled with GENERATED values for named user.
+         * The below parameters are filled with GENERATED values for named user.
          */
-        //StoRM generates a space_token
-        this.spaceToken = (new GUID()).toString();
-        this.spaceFile = "/"+"sf-"+spaceToken;
+        // StoRM generates a space_token
+        spaceToken = (new GUID()).toString();
+        spaceFile = "/" + "sf-" + spaceToken;
 
     }
 
     /**
      * Full constructor.
-     *
+     * 
      * @param maker User
      * @param alias String
      * @param spaceToken String
      * @param path PathName
      */
-    public StorageSpaceTO(GridUserInterface maker, String alias, String spaceToken, String spaceFile)
-    {
-        this.owner = maker;
+    public StorageSpaceTO(GridUserInterface maker, String alias, String spaceToken, String spaceFile) {
+        owner = maker;
         this.alias = alias;
         this.spaceToken = spaceToken;
         this.spaceFile = spaceFile;
     }
 
-
-
-    public StorageSpaceTO(GridUserInterface maker, String type, String alias, String spaceToken, String spaceFile, long guaranteedSize, long totalSize)
-    {
-        this.owner = maker;
-        this.ownerName = maker.getDn();
-        this.voName = getVOName(maker);
-        this.spaceType = type;
+    public StorageSpaceTO(GridUserInterface maker, String type, String alias, String spaceToken,
+            String spaceFile, long guaranteedSize, long totalSize) {
+        owner = maker;
+        ownerName = maker.getDn();
+        voName = getVOName(maker);
+        spaceType = type;
         this.alias = alias;
         this.spaceToken = spaceToken;
         this.spaceFile = spaceFile;
@@ -160,50 +151,47 @@ public class StorageSpaceTO implements Serializable, Comparable {
 
     /**
      * Constructor from Domain Object
-     *
+     * 
      * @param spaceData SpaceData
      */
-    public StorageSpaceTO(it.grid.storm.space.StorageSpaceData spaceData)
-    {
-        if (spaceData!=null)
-        {
-            log.debug("Building StorageSpaceTO with "+spaceData);
-            this.owner = spaceData.getUser();
-            if (spaceData.getUser()!=null)
-            {
-                this.ownerName = (spaceData.getUser()).getDn();
-                this.voName = getVOName(spaceData.getUser());
+    public StorageSpaceTO(it.grid.storm.space.StorageSpaceData spaceData) {
+        if (spaceData != null) {
+            StorageSpaceTO.log.debug("Building StorageSpaceTO with " + spaceData);
+            owner = spaceData.getUser();
+            if (spaceData.getUser() != null) {
+                ownerName = (spaceData.getUser()).getDn();
+                voName = getVOName(spaceData.getUser());
             }
-            if (spaceData.getSpaceType()!=null) {
-                this.spaceType = (spaceData.getSpaceType()).getValue();
+            if (spaceData.getSpaceType() != null) {
+                spaceType = (spaceData.getSpaceType()).getValue();
             }
 
-            this.alias = spaceData.getSpaceTokenAlias();
+            alias = spaceData.getSpaceTokenAlias();
 
-            if (spaceData.getSpaceToken()!=null) {
-                this.spaceToken = spaceData.getSpaceToken().getValue();
+            if (spaceData.getSpaceToken() != null) {
+                spaceToken = spaceData.getSpaceToken().getValue();
             }
 
-            this.spaceFile = spaceData.getSpaceFileNameString();
+            spaceFile = spaceData.getSpaceFileNameString();
 
-            if (spaceData.getGuaranteedSize()!=null) {
-                this.guaranteedSize = spaceData.getGuaranteedSize().value();
+            if (spaceData.getGuaranteedSize() != null) {
+                guaranteedSize = spaceData.getGuaranteedSize().value();
             }
 
-            if (spaceData.getDesiredSize()!=null) {
-                this.totalSize = spaceData.getDesiredSize().value();
+            if (spaceData.getDesiredSize() != null) {
+                totalSize = spaceData.getDesiredSize().value();
             }
 
-            if (spaceData.getUnusedSizes()!=null) {
-                this.unusedSize = spaceData.getUnusedSizes().value();
+            if (spaceData.getUnusedSizes() != null) {
+                unusedSize = spaceData.getUnusedSizes().value();
             }
 
             if (spaceData.getLifeTime() != null) {
-                this.lifetime = spaceData.getLifeTime().value();
+                lifetime = spaceData.getLifeTime().value();
             }
 
             if (spaceData.getDate() != null) {
-                this.created = spaceData.getDate();
+                created = spaceData.getDate();
             }
         }
     }
@@ -212,218 +200,159 @@ public class StorageSpaceTO implements Serializable, Comparable {
     private String getVOName(GridUserInterface maker) {
         String voStr = VO.makeNoVo().getValue();
         if (maker instanceof VomsGridUser) {
-            voStr = ( (VomsGridUser) maker).getVO().getValue();
+            voStr = ((VomsGridUser) maker).getVO().getValue();
         }
         return voStr;
     }
 
     // ********************** Accessor Methods ********************** //
 
-    public Long getStorageSpaceId()
-    {
-        return this.storageSpaceId;
+    public Long getStorageSpaceId() {
+        return storageSpaceId;
     }
 
-    public void setStorageSpaceId(Long id)
-    {
-        this.storageSpaceId = id;
+    public void setStorageSpaceId(Long id) {
+        storageSpaceId = id;
     }
 
-    //-------------------------------------
+    // -------------------------------------
 
-    public GridUserInterface getOwner()
-    {
-        return this.owner;
+    public GridUserInterface getOwner() {
+        return owner;
     }
 
-    public void setOwner(GridUserInterface owner)
-    {
+    public void setOwner(GridUserInterface owner) {
         this.owner = owner;
         // this.ownerName = owner.getDn();
         // this.voName = (owner.getMainVo()).getValue();
     }
 
-    //-------------------------------------
+    // -------------------------------------
 
-    public String getOwnerName()
-    {
-        return this.ownerName;
+    public String getOwnerName() {
+        return ownerName;
     }
 
-    public void setOwnerName(String ownerName)
-    {
+    public void setOwnerName(String ownerName) {
         this.ownerName = ownerName;
     }
 
-    //-------------------------------------
+    // -------------------------------------
 
-    public String getVoName()
-    {
-        return this.voName;
+    public String getVoName() {
+        return voName;
     }
 
-    public void setVoName(String voName)
-    {
+    public void setVoName(String voName) {
         this.voName = voName;
     }
 
-    //-------------------------------------
+    // -------------------------------------
 
-    public String getSpaceType()
-    {
-        return this.spaceType;
+    public String getSpaceType() {
+        return spaceType;
     }
 
-    public void setSpaceType(String spaceType)
-    {
+    public void setSpaceType(String spaceType) {
         this.spaceType = spaceType;
     }
 
-    //-------------------------------------
+    // -------------------------------------
 
-    public long getGuaranteedSize()
-    {
-        return this.guaranteedSize;
+    public long getGuaranteedSize() {
+        return guaranteedSize;
     }
 
-    public void setGuaranteedSize(long guaranteedSize)
-    {
+    public void setGuaranteedSize(long guaranteedSize) {
         this.guaranteedSize = guaranteedSize;
     }
 
-    //-------------------------------------
+    // -------------------------------------
 
-    public long getTotalSize()
-    {
-        return this.totalSize;
+    public long getTotalSize() {
+        return totalSize;
     }
 
-    public void setTotalSize(long totalSize)
-    {
+    public void setTotalSize(long totalSize) {
         this.totalSize = totalSize;
     }
 
-    //-------------------------------------
+    // -------------------------------------
 
-    public long getUnusedSize()
-    {
-        return this.unusedSize;
+    public long getUnusedSize() {
+        return unusedSize;
     }
 
-    public void setUnusedSize(long unusedSize)
-    {
+    public void setUnusedSize(long unusedSize) {
         this.unusedSize = unusedSize;
     }
 
-    //-------------------------------------
+    // -------------------------------------
 
-
-    public void setSpaceToken(String spaceToken)
-    {
+    public void setSpaceToken(String spaceToken) {
         this.spaceToken = spaceToken;
     }
 
-    public String getSpaceToken()
-    {
-        return this.spaceToken;
+    public String getSpaceToken() {
+        return spaceToken;
     }
 
-    //-------------------------------------
+    // -------------------------------------
 
-    public void setAlias(String alias)
-    {
+    public void setAlias(String alias) {
         this.alias = alias;
     }
 
-    public String getAlias()
-    {
-        return this.alias;
+    public String getAlias() {
+        return alias;
     }
 
-    //-------------------------------------
+    // -------------------------------------
 
-    public void setSpaceFile(String spaceFile)
-    {
+    public void setSpaceFile(String spaceFile) {
         this.spaceFile = spaceFile;
     }
 
-    public String getSpaceFile()
-    {
-        return this.spaceFile;
+    public String getSpaceFile() {
+        return spaceFile;
     }
 
-    //-------------------------------------
+    // -------------------------------------
 
-    public long getLifetime()
-    {
-        return this.lifetime;
+    public long getLifetime() {
+        return lifetime;
     }
 
-    public void setLifetime(long lifetime)
-    {
+    public void setLifetime(long lifetime) {
         this.lifetime = lifetime;
     }
 
-    //-------------------------------------
+    // -------------------------------------
 
-    public String getStorageInfo()
-    {
-        return this.storageInfo;
+    public String getStorageInfo() {
+        return storageInfo;
     }
 
-    public void setStorageInfo(String storageInfo)
-    {
+    public void setStorageInfo(String storageInfo) {
         this.storageInfo = storageInfo;
     }
 
-    //-------------------------------------
-
-    public void setStorageFiles(Set storageFiles)
-    {
-        this.storageFiles = storageFiles;
-    }
-
-    public Set getStorageFiles()
-    {
-        return this.storageFiles;
-    }
-
-    public void addStorageFile(StorageFileTO storageFile)
-    {
-        if (storageFile==null)
-        {
-            throw new IllegalArgumentException("Can't add a null actionsEntry!");
-        }
-        //This setting is for bi-directional association
-        storageFile.setStorageSpace(this);
-        this.getStorageFiles().add(storageFile);
-    }
-
-    public void removeFile(StorageFileTO storageFile) {
-        /**
-         * @todo To check the consistency of set..
-         **/
-        getStorageFiles().remove(storageFile);
-    }
-
-    //-------------------------------------
+    // -------------------------------------
 
     public Date getCreated() {
         return created;
     }
 
     public void setCreated(Date date) {
-        this.created = date;
+        created = date;
     }
+
     // ********************** Common Methods ********************** //
 
     @Override
     public boolean equals(Object o) {
         if (this == o) {
             return true;
-        }
-        if (!(o instanceof StorageFileTO)) {
-            return false;
         }
         final StorageSpaceTO storageSpace = (StorageSpaceTO) o;
         if (!spaceToken.equals(storageSpace.getSpaceToken())) {
@@ -447,27 +376,41 @@ public class StorageSpaceTO implements Serializable, Comparable {
     @Override
     public String toString() {
         StringBuffer sb = new StringBuffer();
-        sb.append(" ==== STORAGE SPACE (token="+spaceToken+") ==== \n");
-        sb.append(" STORAGE SPACE ID = "+storageSpaceId); sb.append("\n");
-        sb.append(" OWNER USER NAME  = "+ownerName); sb.append("\n");
-        sb.append(" OWNER VO NAME    = "+voName); sb.append("\n");
-        sb.append(" SPACE ALIAS NAME = "+alias); sb.append("\n");
-        sb.append(" SPACE TYPE       = "+spaceType); sb.append("\n");
-        sb.append(" SPACE TOKEN      = "+spaceToken); sb.append("\n");
-        sb.append(" SPACE FILE       = "+spaceFile); sb.append("\n");
-        sb.append(" CREATED          = "+created); sb.append("\n");
-        sb.append(" TOTAL SIZE       = "+totalSize); sb.append("\n");
-        sb.append(" GUARANTEED SIZE  = "+guaranteedSize); sb.append("\n");
-        sb.append(" FREE SIZE        = "+unusedSize); sb.append("\n");
-        sb.append(" LIFETIME (sec)   = "+lifetime); sb.append("\n");
-        sb.append(" STORAGE INFO     = "+storageInfo); sb.append("\n");
-        sb.append(" NR STOR_FILES    = <UNDEF for NOW..>"); sb.append("\n");
+        sb.append(" ==== STORAGE SPACE (token=" + spaceToken + ") ==== \n");
+        sb.append(" STORAGE SPACE ID = " + storageSpaceId);
+        sb.append("\n");
+        sb.append(" OWNER USER NAME  = " + ownerName);
+        sb.append("\n");
+        sb.append(" OWNER VO NAME    = " + voName);
+        sb.append("\n");
+        sb.append(" SPACE ALIAS NAME = " + alias);
+        sb.append("\n");
+        sb.append(" SPACE TYPE       = " + spaceType);
+        sb.append("\n");
+        sb.append(" SPACE TOKEN      = " + spaceToken);
+        sb.append("\n");
+        sb.append(" SPACE FILE       = " + spaceFile);
+        sb.append("\n");
+        sb.append(" CREATED          = " + created);
+        sb.append("\n");
+        sb.append(" TOTAL SIZE       = " + totalSize);
+        sb.append("\n");
+        sb.append(" GUARANTEED SIZE  = " + guaranteedSize);
+        sb.append("\n");
+        sb.append(" FREE SIZE        = " + unusedSize);
+        sb.append("\n");
+        sb.append(" LIFETIME (sec)   = " + lifetime);
+        sb.append("\n");
+        sb.append(" STORAGE INFO     = " + storageInfo);
+        sb.append("\n");
+        sb.append(" NR STOR_FILES    = <UNDEF for NOW..>");
+        sb.append("\n");
         return sb.toString();
     }
 
     public int compareTo(Object o) {
-        if (o instanceof StorageFileTO) {
-            return this.getCreated().compareTo(((StorageSpaceTO) o).getCreated());
+        if (o instanceof StorageSpaceTO) {
+            return getCreated().compareTo(((StorageSpaceTO) o).getCreated());
         }
         return 0;
     }
