136e32fac9588381b107a495472593230ffba3bc
Alberto Forti <alberto.forti@cnaf.infn.it>
2010-02-16 16:23:26 +0000

    devel lcmaps with jna

--- src/it/grid/storm/jna/StormLCMAPS.java
+++ src/it/grid/storm/jna/StormLCMAPS.java
@@ -17,55 +17,36 @@ public class StormLCMAPS {
         StormLcmapsLibrary INSTANCE = (StormLcmapsLibrary) Native.loadLibrary(("storm_lcmaps"),
                                                                               StormLcmapsLibrary.class);
 
-
         int init_lcmaps();
-        // void map_user(const char *user_dn, const char **fqan_list, int nfqan, int *uid, int **gids, int *ngids)
+
         int map_user(String user_dn, String[] fqan_list, int nfqan, IntByReference uid,
                 PointerByReference gids, IntByReference ngids);
+
         void free_gids(PointerByReference gids);
     }
 
     private static final Logger log = LoggerFactory.getLogger(StormLCMAPS.class);
 
-    public static void test() {
-        PointerByReference gids = new PointerByReference();
-        IntByReference uid = new IntByReference();
-        IntByReference ngids = new IntByReference();
-
-        String[] fqanArray = new String[2];
-        fqanArray[0] = "fqan0";
-        fqanArray[1] = "fqan1";
-        StormLcmapsLibrary.INSTANCE.map_user("Alberto", fqanArray, fqanArray.length, uid, gids, ngids);
-
-        Pointer p = gids.getValue();
-        int[] gidsArray = p.getIntArray(0, ngids.getValue());
-        
-        System.out.println("uid  : " + uid.getValue());
-        System.out.println("ngids: " + gidsArray.length);
-        for (int gid:gidsArray) {
-            System.out.println("  gid[" + gid + "]: " + gid);
-        }
-        
-        System.out.println();
-        System.out.println("Game over");
-
-    }
-
+    /*
+     * To be used for test purposes. In order to correctly initialize the lcmaps library be sure the following
+     * environment variable are set: LCMAPS_LOG_FILE (path to the log file) and LCMAPS_DB_FILE (path to the lcmaps db
+     * file).
+     */
     public static void main(String[] args) {
 
         if (args.length != 1) {
             System.out.println("usage: <dn> [[fqan]... [fqan]");
             System.exit(1);
         }
-        
+
         String dn = args[0];
         String[] fqanArray = new String[args.length - 1];
-        
-        for (int i = 1; i< args.length; i++) {
-            fqanArray[i-1] = args[i];
+
+        for (int i = 1; i < args.length; i++) {
+            fqanArray[i - 1] = args[i];
             System.out.println("fqan: " + fqanArray[i]);
         }
-        
+
         IntByReference uid = new IntByReference();
         IntByReference ngids = new IntByReference();
         PointerByReference gids = new PointerByReference();
@@ -75,7 +56,7 @@ public class StormLCMAPS {
         if (rc != 0) {
             System.exit(2);
         }
-        
+
         rc = StormLcmapsLibrary.INSTANCE.map_user(dn, fqanArray, fqanArray.length, uid, gids, ngids);
 
         System.out.println("Return code map_user: " + rc);
@@ -85,16 +66,15 @@ public class StormLCMAPS {
 
         System.out.println("uid: " + uid.getValue());
         System.out.println("Number of gids: " + ngids.getValue());
-        
+
         Pointer p = gids.getValue();
         int[] gidsArray = p.getIntArray(0, ngids.getValue());
 
-        for (int gid:gidsArray) {
+        for (int gid : gidsArray) {
             System.out.println("  gid[" + gid + "]: " + gid);
-        }        
+        }
 
         StormLcmapsLibrary.INSTANCE.free_gids(gids);
 
     }
 }
-
--- src/it/grid/storm/jna/lcmaps/storm_lcmaps.c
+++ src/it/grid/storm/jna/lcmaps/storm_lcmaps.c
@@ -28,76 +28,101 @@ extern "C" {
 
 const char* LCMAPS_LOG_FILE = "LCMAPS_LOG_FILE";
 
+/******************************************************************************
+ * Description:
+ * Initializes the lcmaps library. This function relies on lcmaps environment
+ * variables, specifically: LCMAPS_DB_FILE and LCMAPS_LOG_FILE.
+ *
+ * Returns:
+ * 0: success
+ * any value other than 0: failure
+ ******************************************************************************/
 int init_lcmaps() {
 
 	char* log_file_name = getenv(LCMAPS_LOG_FILE);
 	unsetenv(LCMAPS_LOG_FILE);
 
-        setenv("LCMAPS_DB_FILE", "/opt/storm/backend/etc/lcmaps.db", 1);
-
-        log_file_name = "alb-lcmaps.log";
-
-	int rc = lcmaps_init_and_logfile(log_file_name, NULL, (unsigned short) 0x0001);
+	int rc = lcmaps_init_and_logfile(log_file_name, NULL,
+			(unsigned short) 0x0001);
 
 	return rc;
 }
 
+/******************************************************************************
+ * Description:
+ * Maps Grid credentials to a local user.
+ *
+ * Input parameters:
+ * user_dn: DN
+ * fqan_list: FQAN list
+ * nfqan: number of elements of the fqan_list list.
+ *
+ * Output parameters:
+ * uid: uid of the local user.
+ * gids: list of primary and secondary gids of the local user.
+ * ngids: number of elements of the gids list.
+ *
+ * Returns:
+ * 0: success
+ * any value other than 0: failure
+ ******************************************************************************/
 int map_user(const char *user_dn, const char **fqan_list, int nfqan, int *uid,
 		void **gids, int *ngids) {
 
 	lcmaps_account_info_t lcmaps_account;
 	lcmaps_account_info_init(&lcmaps_account);
 
-	//int rc = lcmaps_return_poolindex_without_gsi(const_cast<char*>(user_dn), const_cast<char**>(fqan_list), nfqan, &lcmaps_account);
+	int rc = lcmaps_return_account_without_gsi(const_cast<char*>(user_dn), const_cast<char**>(fqan_list), nfqan, 0, &lcmaps_account);
+	if (rc != 0) {
+		lcmaps_account_info_clean(&lcmaps_account);
+		return rc;
+	}
 
-        int rc = lcmaps_return_account_without_gsi(const_cast<char*>(user_dn), const_cast<char**>(fqan_list), nfqan, 0, &lcmaps_account);
-        if (rc != 0) {
-            lcmaps_account_info_clean(&lcmaps_account);
-            return rc;
-        }
+	int npgid = lcmaps_account.npgid;
+	int nsgid = lcmaps_account.nsgid;
+	int arraySize = npgid + nsgid;
 
-        int npgid = lcmaps_account.npgid;
-        int nsgid = lcmaps_account.nsgid;
-        int arraySize = npgid + nsgid;
+	// Set output data uid and ngids
+	*uid = lcmaps_account.uid;
+	*ngids = arraySize;
 
-        // Set output data uid and ngids
-        *uid = lcmaps_account.uid;
-        *ngids = arraySize;
-        
-         if (arraySize == 0) {
-             *gids = NULL;
-              lcmaps_account_info_clean(&lcmaps_account);
-             return rc;
-         }
+	if (arraySize == 0) {
+		*gids = NULL;
+		lcmaps_account_info_clean(&lcmaps_account);
+		return rc;
+	}
 
-        // Alloc memory for the array of gids in the output data gids
-        *gids = malloc(sizeof(int) * arraySize);
+	// Alloc memory for the array of gids in the output data gids
+	*gids = malloc(sizeof(int) * arraySize);
 
-        int* gidArray = (int*) *gids;
+	int* gidArray = (int*) *gids;
 
-        // Copy primary gids first
-        int index = 0;
-        int* sourceArray;
+	// Copy primary gids first
+	int index = 0;
+	int* sourceArray;
 
-        if (npgid > 0) {
-            sourceArray = (int*) lcmaps_account.pgid_list;
-            for (; index < npgid; index++) {
-                gidArray[index] = sourceArray[index];
-            }
-        }
+	if (npgid > 0) {
+		sourceArray = (int*) lcmaps_account.pgid_list;
+		for (; index < npgid; index++) {
+			gidArray[index] = sourceArray[index];
+		}
+	}
 
-        if (nsgid > 0) {
-            sourceArray = (int*) lcmaps_account.sgid_list;
-            for (int i = 0; index < arraySize; index++, i++) {
-                gidArray[index] = sourceArray[i];
-            }
-        }
+	if (nsgid > 0) {
+		sourceArray = (int*) lcmaps_account.sgid_list;
+		for (int i = 0; index < arraySize; index++, i++) {
+			gidArray[index] = sourceArray[i];
+		}
+	}
 
 	lcmaps_account_info_clean(&lcmaps_account);
 	return rc;
 }
 
+/******************************************************************************
+ * Description:
+ * Frees the list of gids allocated by the map_user function.
+ ******************************************************************************/
 void free_gids(void **p) {
 	free(*p);
 }
-
