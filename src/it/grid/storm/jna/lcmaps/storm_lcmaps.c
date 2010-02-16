8993f151a1d71f0bd57655ff2eddd613e03f68a3
Alberto Forti <alberto.forti@cnaf.infn.it>
2010-02-16 13:19:50 +0000

    devel

--- src/it/grid/storm/jna/lcmaps/storm_lcmaps.c
+++ src/it/grid/storm/jna/lcmaps/storm_lcmaps.c
@@ -1,3 +1,17 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -10,43 +24,44 @@ extern "C" {
 }
 
 #include <stdlib.h>
+#include "storm_lcmaps.h"
 
-extern "C" void map_user(const char *user_dn, const char **fqan_list, int nfqan, int *uid, int **gids, int *ngids) {
+const char* LCMAPS_LOG_FILE = "LCMAPS_LOG_FILE";
 
-   *uid=14;
-   *gids = (int *) malloc(sizeof(int) * 10);
+int init_lcmaps() {
 
-    int i;
-    for (i=0; i<10; i++) {
-        *gids[i] = i;
-    }
+	char* log_file_name = getenv(LCMAPS_LOG_FILE);
+	unsetenv(LCMAPS_LOG_FILE);
 
-/*
-    lcmaps_init_and_logfile("alb-lcmaps.log", NULL, (unsigned short)0x0001);
-    lcmaps_account_info_t plcmaps_account;
-    lcmaps_account_info_init(&plcmaps_account);
+	int rc = lcmaps_init_and_logfile(log_file_name, NULL, (unsigned short) 0x0001);
 
-    int rc = lcmaps_return_poolindex_without_gsi(const_cast<char*>(user_dn), const_cast<char**>(fqan_list), nfqan, &plcmaps_account);
-
-    printf("Result code: %d\n", rc);
-    printf("uid: %d\n", plcmaps_account.uid);
-
-    lcmaps_account_info_clean(&plcmaps_account);
-*/
-
+	return rc;
 }
 
-extern "C" void prova(void **gids) {
+int map_user(const char *user_dn, const char **fqan_list, int nfqan, int *uid,
+		void **gids, int *ngids) {
 
-    *gids =  malloc(sizeof(int) * 10);
+	*uid = 14;
+	*gids = (int *) malloc(sizeof(int) * 10);
 
-     int *p = (int*) *gids;
+	int i;
+	for (i = 0; i < 10; i++) {
+		*gids[i] = i;
+	}
 
-     printf("2");
+	lcmaps_account_info_t plcmaps_account;
+	lcmaps_account_info_init(&plcmaps_account);
 
-     int i;
-    for (i=0; i<10; i++) {
-        p[i] = 1000-i;
-    }
+	int rc = lcmaps_return_poolindex_without_gsi(const_cast<char*>(user_dn), const_cast<char**>(fqan_list), nfqan, &plcmaps_account);
+
+	printf("Result code: %d\n", rc);
+	printf("uid: %d\n", plcmaps_account.uid);
+
+	lcmaps_account_info_clean(&plcmaps_account);
+
+	return rc;
 }
 
+void free_gids(void **p) {
+	free(*p);
+}
