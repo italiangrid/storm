1bf3cc512d2ddda6f4fa9a8c6484eaaa023513fc
Alberto Forti <alberto.forti@cnaf.infn.it>
2010-02-17 10:26:25 +0000

    devel lcmaps on jna

--- src/it/grid/storm/jna/StormLcmapsJna.java
+++ src/it/grid/storm/jna/StormLcmapsJna.java
@@ -1,5 +1,8 @@
 package it.grid.storm.jna;
 
+import it.grid.storm.griduser.CannotMapUserException;
+import it.grid.storm.griduser.LocalUser;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,7 +12,7 @@ import com.sun.jna.Pointer;
 import com.sun.jna.ptr.IntByReference;
 import com.sun.jna.ptr.PointerByReference;
 
-public class StormLCMAPS {
+public class StormLcmapsJna {
 
     /* Functions implemented by libstorm_lcmaps.so */
     private interface StormLcmapsLibrary extends Library {
@@ -17,16 +20,30 @@ public class StormLCMAPS {
         StormLcmapsLibrary INSTANCE = (StormLcmapsLibrary) Native.loadLibrary(("storm_lcmaps"),
                                                                               StormLcmapsLibrary.class);
 
+        void free_gids(PointerByReference gids);
+
         int init_lcmaps();
 
         int map_user(String user_dn, String[] fqan_list, int nfqan, IntByReference uid,
                 PointerByReference gids, IntByReference ngids);
-
-        void free_gids(PointerByReference gids);
     }
-
-    private static final Logger log = LoggerFactory.getLogger(StormLCMAPS.class);
-
+    
+    private static final Logger log = LoggerFactory.getLogger(StormLcmapsJna.class);
+    /** To synchronize on LCMAPS invocation. */
+    private static Object lock = new Object();
+    
+    static {
+        if (StormLcmapsLibrary.INSTANCE.init_lcmaps() != 0) {
+            String lcmapsLogFile = System.getenv("LCMAPS_LOG_FILE");
+            
+            if (lcmapsLogFile == null) {
+                lcmapsLogFile = "";
+            }
+            
+            log.error("Error while initializing LCMAPS, see LCMAPS logfile: " + lcmapsLogFile);
+        }
+    }
+    
     /*
      * To be used for test purposes. In order to correctly initialize the lcmaps library be sure the following
      * environment variable are set: LCMAPS_LOG_FILE (path to the log file) and LCMAPS_DB_FILE (path to the lcmaps db
@@ -76,5 +93,28 @@ public class StormLCMAPS {
 
         StormLcmapsLibrary.INSTANCE.free_gids(gids);
 
+    }
+
+    public LocalUser map(final String dn, final String[] fqans) throws CannotMapUserException {
+        
+        IntByReference uid = new IntByReference();
+        IntByReference ngids = new IntByReference();
+        PointerByReference gids = new PointerByReference();
+
+        int rc;
+        synchronized (lock) {
+            rc = StormLcmapsLibrary.INSTANCE.map_user(dn, fqans, fqans.length, uid, gids, ngids);
+        }
+        
+        if (rc != 0) {
+            throw new CannotMapUserException("LCMAPS error, cannot map user credentials to local user.");
+        }
+        
+        Pointer p = gids.getValue();
+        LocalUser localUser = new LocalUser(uid.getValue(), p.getIntArray(0, ngids.getValue()), ngids.getValue());
+        
+        StormLcmapsLibrary.INSTANCE.free_gids(gids);
+        
+        return localUser;
     }
 }
