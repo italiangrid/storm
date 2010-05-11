43bdfaade6f909108aa21703aa2f76a362c9aadf
Riccardo Zappi <riccardo.zappi@cnaf.infn.it>
2010-05-11 18:10:50 +0000

    Fixes of sometypo error on unit test

--- unittest/component/namespace/userinfo/RetrieveGroupInfo.java
+++ unittest/component/namespace/userinfo/RetrieveGroupInfo.java
@@ -1,16 +1,22 @@
 package component.namespace.userinfo;
 
 import it.grid.storm.config.Configuration;
-import it.grid.storm.startup.Bootstrap;
+
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import component.utiltest.SetUpTest;
+
+
 public class RetrieveGroupInfo {
 
-    
+    private static Logger log = LoggerFactory.getLogger(RetrieveGroupInfo.class);
 
     /**
      * @param args
@@ -21,8 +27,8 @@ public class RetrieveGroupInfo {
             configurationDir = System.getProperty("user.dir") + File.separator + "unittest" + File.separator;
         }
         String logFile = configurationDir + "logging-test.xml";
-
-        Bootstrap.initializeLogging(logFile);
+        SetUpTest.init(logFile);
+         
         //getent group storm  | awk -F"," '{print $1}'| awk -F":" '{print $3}'
         try { // Execute a command with an argument that contains a space 
             String[] commands = new String[]{"getent", "group", "ritz"}; 
@@ -30,39 +36,43 @@ public class RetrieveGroupInfo {
             
             BufferedReader stdInput = new BufferedReader(new InputStreamReader(child.getInputStream()));
             //process the Command Output (Input for StoRM ;) )
-            String line;
-            int row = 0;
-            System.out.println("UserInfo Command Output :");
-            while ( (line = stdInput.readLine()) != null) {
-                System.out.print(line);
-            }
+            String line = null;
+  
+            log.info("UserInfo Command Output :");
+            line = stdInput.readLine();
             
-            String patternStr = ":"; 
-            String[] fields = inputStr.split(patternStr); 
-           
             String groupName = null;
             String gid = null;
             int gidInt = -1;
             
-            if (fields[0]!=null) {
-                System.out.println("field[0], group name ='"+groupName+"'");
-                groupName = fields[0];
-            } 
-            if ((fields.length>1) and (fields[1]!=null) {
-               System.out.println("field[1], encrypted group password (or x if shadow passwords are in use) ='"+fields[1]+"'");
+            String patternStr = ":"; 
+            String[] fields = null;
+            if (line!=null) {
+                log.info("LINE = "+line);
+                fields = line.split(patternStr); 
+                if (fields[0]!=null) {
+                    groupName = fields[0];
+                    log.debug("field[0], group name ='"+groupName+"'");
+                } 
+                if ((fields.length>1) && (fields[1]!=null)) {
+                   log.debug("field[1], encrypted group password (or x if shadow passwords are in use) ='"+fields[1]+"'");
+                }
+                if ((fields.length>2) && (fields[2]!=null) ){  
+                   gid = fields[2];   
+                   log.debug("field[2], GID ='"+gid+"'");
+                   try {
+                     gidInt = Integer.parseInt(gid);
+                   } catch (NumberFormatException nfe) {
+                       log.error("Unable to retrieve the GID number of groupName '"+groupName+"'");
+                   }
+                }
+                if ((fields.length>3) && (fields[3]!=null)) {
+                    log.debug("field[3], group members' usernames, comma-separated ='"+fields[3]+"'");
+                }      
+                    
             }
-            if ((fields.length>2) and (fields[2]!=null) {  
-               gid = fields[2];   
-               System.out.println("field[2], GID ='"+gid+"'");
-               try {
-                 gidInt = Integer.parseInt(gid);
-               } catch (NumberFormatException nfe) {
-                   log.error("Unable to retrieve the GID number of groupName '"+groupName+"'");
-               }
-            }
-            if ((fields.length>3) and (fields[3]!=null) {
-                System.out.println("field[3], group members' usernames, comma-separated ='"+fields[3]+"'");
-            }      
+            
+                        
         } catch (IOException e) { 
             log.error("Unable to retrieve the GID number ");
         } 
