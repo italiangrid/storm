ffe7708e761e2c86582148fdce16f0bb88ae4531
Riccardo Zappi <riccardo.zappi@cnaf.infn.it>
2010-05-12 15:02:56 +0000

    Preparation of 1.5.2 version

--- etc/db/storm_mysql_update_from_1.5.x_to_1.5.2.sql
+++ etc/db/storm_mysql_update_from_1.5.x_to_1.5.2.sql
@@ -4,4 +4,4 @@
 
 REPLACE INTO storm_db.db_version (major,minor,revision,description) VALUES (1,5,2,'15 May 2010');
 
-ALTER TABLE storm_be_ISAM.tape_recall modify taskId int NOT NULL; 
+ALTER TABLE storm_be_ISAM.tape_recall modify taskId CHAR(36) NOT NULL; 
