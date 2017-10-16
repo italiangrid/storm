a21a1ba8e8cf363704c2ec1b9fd65c5ce456e02e
enricovianello <maverick17586@gmail.com>
2017-10-16 10:02:34 +0200

    Removed PingCommand POST test on recall table
    Removed old and unused stuff

--- src/main/java/it/grid/storm/tape/recalltable/persistence/TapeRecallBuilder.java
+++ src/main/java/it/grid/storm/tape/recalltable/persistence/TapeRecallBuilder.java
@@ -20,134 +20,29 @@
  */
 package it.grid.storm.tape.recalltable.persistence;
 
-import it.grid.storm.persistence.model.TapeRecallTO;
-import it.grid.storm.srm.types.InvalidTRequestTokenAttributesException;
-import it.grid.storm.srm.types.TRequestToken;
-import it.grid.storm.tape.recalltable.model.TapeRecallData;
-import it.grid.storm.tape.recalltable.model.TapeRecallStatus;
-
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.UUID;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * @author zappi
  * 
  */
 public class TapeRecallBuilder {
 
-	private static final Logger log = LoggerFactory
-		.getLogger(TapeRecallBuilder.class);
-
 	/**
 	 * { "filename":"<file-name>"; "dn":"<DN>"; "fqans":["fqan":"<FQAN>",
 	 * "fqan":"<FQAN>"]; "vo-name":"<vo-name>" }
 	 **/
-	public static final String taskStart = "{";
-	public static final String taskEnd = "}";
-	public static final String elementSep = " # ";
-	public static final String fnPrefix = "filename";
-	public static final String dnPrefix = "dn";
-	public static final String fqansPrefix = "fqans";
-	public static final String fqansArrayStart = "[";
-	public static final String fqansArrayEnd = "]";
-	public static final String fqanPrefix = "fqan";
-	public static final String fqanSep = ",";
-	public static final String voNamePrefix = "vo-name";
-	public static final String userIdPrefix = "userId";
-	public static final String equalChar = "=";
+	public static final String TASK_START = "{";
+	public static final String TASK_END = "}";
+	public static final String ELEMENT_SEP = " # ";
+	public static final String FN_PREFIX = "filename";
+	public static final String DN_PREFIX = "dn";
+	public static final String FQANS_PREFIX = "fqans";
+	public static final String FQANS_ARRAY_START = "[";
+	public static final String FQANS_ARRAY_END = "]";
+	public static final String FQAN_PREFIX = "fqan";
+	public static final String FQAN_SEP = ",";
+	public static final String VONAME_PREFIX = "vo-name";
+	public static final String USERID_PREFIX = "userId";
+	public static final String EQUAL_CHAR = "=";
 
-	/**
-	 * 
-	 * @param bodyInput
-	 *          : 4 fields "stfn, dn, fqans, vo-name"
-	 * @return
-	 */
-	public static TapeRecallTO buildFromPOST(TapeRecallData rtd) {
-
-		// Create a new RecallTaskTO
-		TapeRecallTO task = new TapeRecallTO();
-
-		Date currentDate = new Date();
-		task.setInsertionInstant(currentDate);
-
-		task.setRequestType(TapeRecallTO.TRequestType.BACK_REQUEST);
-
-		String localRequestToken = "local-" + UUID.randomUUID();
-		TRequestToken localRT;
-		try {
-			localRT = new TRequestToken(localRequestToken, Calendar.getInstance()
-				.getTime());
-		} catch (InvalidTRequestTokenAttributesException e) {
-			// unexpected
-			throw new IllegalStateException(
-				"Unexpected InvalidTRequestTokenAttributesException: " + e);
-		}
-		task.setRequestToken(localRT);
-
-		task.setPinLifetime(-1);
-		task.setDeferredRecallInstant(currentDate);
-
-		task.setUserID(rtd.getUserID());
-		task.setVoName(rtd.getVoName());
-
-		// Setting values into RecallTaskTO
-		// - note: the setting of filename triggers the building of taskId
-		task.setFileName(rtd.getFileName());
-
-		return task;
-	}
-
-	/**
-	 * @param string
-	 * @return
-	 */
-	private static TapeRecallStatus parseTaskStatus(String taskStatus) {
-
-		TapeRecallStatus result = TapeRecallStatus.UNDEFINED;
-		try {
-			result = TapeRecallStatus.getRecallTaskStatus(taskStatus);
-		} catch (Exception e) {
-			log.error("Task Status '{}' is not in a valid format." , taskStatus , e);
-		}
-		return result;
-	}
-
-	/**
-	 * @param string
-	 * @return
-	 */
-	private static int parseInt(String number) {
-
-		int result = 0;
-		try {
-			result = Integer.parseInt(number);
-		} catch (Exception e) {
-			log.error("Number '{}' is not in a valid format." , number , e);
-		}
-		return result;
-	}
-
-	/**
-	 * @param string
-	 * @return
-	 */
-	private static Date parseDate(String dateString) {
-
-		DateFormat formatter = new SimpleDateFormat(TapeRecallTO.DATE_FORMAT);
-		Date date = new Date();
-		try {
-			date = formatter.parse(dateString);
-		} catch (ParseException e) {
-			log.error("Date '{}' is not in a valid format." , dateString , e);
-		}
-		return date;
-	}
-
+	private TapeRecallBuilder() {}
 }
