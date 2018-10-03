d55f9fa85247b69aab2b1bedf225299793a0bc79
enricovianello <maverick17586@gmail.com>
2018-10-03 16:13:39 +0200

    Removed unused code and related dependencies

---
+++ src/main/java/it/grid/storm/asynch/UnsupportedOperationFeeder.java
@@ -0,0 +1,20 @@
+package it.grid.storm.asynch;
+
+import it.grid.storm.scheduler.Delegable;
+
+public class UnsupportedOperationFeeder implements Delegable {
+
+  @Override
+  public void doIt() {
+    
+    throw new UnsupportedOperationException();
+
+  }
+
+  @Override
+  public String getName() {
+
+    return "Unsupported Operation Feeder";
+  }
+
+}
