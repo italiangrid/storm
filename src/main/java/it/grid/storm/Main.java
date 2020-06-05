12fd5bae0f8479dc06ad211d8b4583bbb94b4566
enricovianello <maverick17586@gmail.com>
2020-06-05 15:20:58 +0200

    Moved StoRM init before intercept any exceptions gracefully

--- src/main/java/it/grid/storm/Main.java
+++ src/main/java/it/grid/storm/Main.java
@@ -11,9 +11,9 @@ public class Main {
 
   public static void main(String[] args) {
 
-    Thread.setDefaultUncaughtExceptionHandler(new StoRMDefaultUncaughtExceptionHandler());
-
     StoRM storm = new StoRM();
+
+    Thread.setDefaultUncaughtExceptionHandler(new StoRMDefaultUncaughtExceptionHandler());
 
     Runtime.getRuntime().addShutdownHook(new ShutdownHook(storm));
 
