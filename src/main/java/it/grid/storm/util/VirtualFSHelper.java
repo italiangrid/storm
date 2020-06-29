be444beb3e4f9d51b7615230b5945262161968bc
enricovianello <maverick17586@gmail.com>
2020-06-29 16:52:21 +0200

    Formatted some files

--- src/main/java/it/grid/storm/util/VirtualFSHelper.java
+++ src/main/java/it/grid/storm/util/VirtualFSHelper.java
@@ -11,50 +11,50 @@ import java.util.Collection;
 import java.util.List;
 
 public class VirtualFSHelper {
-	
-	public static final boolean isGPFSQuotaEnabledForVFS(VirtualFSInterface vfs) {
 
-		boolean result = false;
-	
-		if (vfs != null) {
-			CapabilityInterface cap = null;
-			Quota quota = null;
-			String fsType = "Unknown";
-			fsType = vfs.getFSType();
-			if (fsType != null) {
-				if (fsType.trim().toLowerCase().equals("gpfs")) {
-					cap = vfs.getCapabilities();
-					if (cap != null) {
-						quota = cap.getQuota();
-					}
-					if (quota != null) {
-						result = ((quota.getDefined()) && (quota.getEnabled()));
-					}
-				}
-			}
-		}
-		return result;
-	}
-	
-	public static List<VirtualFSInterface> getGPFSQuotaEnabledFilesystems(){
-		
-		List<VirtualFSInterface> fss = new ArrayList<VirtualFSInterface>();
-		
-		Collection<VirtualFSInterface> allVFS;
-		try {
-			allVFS = NamespaceDirector.getNamespace().getAllDefinedVFS();
-		
-		} catch (NamespaceException e1) {
-			
-			throw new IllegalStateException(e1);
-		}
-		
-		for (VirtualFSInterface vfs : allVFS){
-			if (isGPFSQuotaEnabledForVFS(vfs))
-				fss.add(vfs);
-		}
-		
-		return fss;
-		
-	}
+  public static final boolean isGPFSQuotaEnabledForVFS(VirtualFSInterface vfs) {
+
+    boolean result = false;
+
+    if (vfs != null) {
+      CapabilityInterface cap = null;
+      Quota quota = null;
+      String fsType = "Unknown";
+      fsType = vfs.getFSType();
+      if (fsType != null) {
+        if (fsType.trim().toLowerCase().equals("gpfs")) {
+          cap = vfs.getCapabilities();
+          if (cap != null) {
+            quota = cap.getQuota();
+          }
+          if (quota != null) {
+            result = ((quota.getDefined()) && (quota.getEnabled()));
+          }
+        }
+      }
+    }
+    return result;
+  }
+
+  public static List<VirtualFSInterface> getGPFSQuotaEnabledFilesystems() {
+
+    List<VirtualFSInterface> fss = new ArrayList<VirtualFSInterface>();
+
+    Collection<VirtualFSInterface> allVFS;
+    try {
+      allVFS = NamespaceDirector.getNamespace().getAllDefinedVFS();
+
+    } catch (NamespaceException e1) {
+
+      throw new IllegalStateException(e1);
+    }
+
+    for (VirtualFSInterface vfs : allVFS) {
+      if (isGPFSQuotaEnabledForVFS(vfs))
+        fss.add(vfs);
+    }
+
+    return fss;
+
+  }
 }
