36853c2110b529b145fc0f34baaf7a2c1e0e4486
enricovianello <maverick17586@gmail.com>
2020-08-05 15:02:12 +0200

    Converted all dos files to unix

--- src/main/java/it/grid/storm/filesystem/MockSpaceSystem.java
+++ src/main/java/it/grid/storm/filesystem/MockSpaceSystem.java
@@ -23,9 +23,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Class that represents a SpaceSystem that always acknowledges a successful
- * space reservation; it is meant to be used with filesystems that do not
- * support natively space reservation operations. It acts as a mock object!
+ * Class that represents a SpaceSystem that always acknowledges a successful space reservation; it
+ * is meant to be used with filesystems that do not support natively space reservation operations.
+ * It acts as a mock object!
  * 
  * @author EGRID - ICTP Trieste
  * @version 1.0
@@ -33,78 +33,76 @@ import org.slf4j.LoggerFactory;
  */
 public class MockSpaceSystem implements SpaceSystem {
 
-	private String mountpoint = ""; // String representing the local mount point,
-																	// that is the root from which This
-																	// SpaceSystem operates!
-	private static Logger log = LoggerFactory.getLogger(MockSpaceSystem.class);
+  private String mountpoint = ""; // String representing the local mount point,
+                                  // that is the root from which This
+                                  // SpaceSystem operates!
+  private static Logger log = LoggerFactory.getLogger(MockSpaceSystem.class);
 
-	public MockSpaceSystem(String mountpoint) throws SpaceSystemException {
+  public MockSpaceSystem(String mountpoint) throws SpaceSystemException {
 
-		if (mountpoint == null) {
-			throw new SpaceSystemException("Supplied mountpoint is null!");
-		}
-		this.mountpoint = mountpoint;
-	}
+    if (mountpoint == null) {
+      throw new SpaceSystemException("Supplied mountpoint is null!");
+    }
+    this.mountpoint = mountpoint;
+  }
 
-	/**
-	 * Method that follows the contract specified in SpaceSystem Interface: please
-	 * refer there for further info.
-	 */
-	public long reserveSpace(String pathToFile, long size)
-		throws ReservationException {
+  /**
+   * Method that follows the contract specified in SpaceSystem Interface: please refer there for
+   * further info.
+   */
+  public long reserveSpace(String pathToFile, long size) throws ReservationException {
 
-		java.io.File localFile = new java.io.File(pathToFile);
-		try {
-			localFile.createNewFile();
-		} catch (IOException e) {
-		  log.error(e.getMessage(), e);
-			throw new ReservationException(
-				"IO exception while creating local File named : " + pathToFile,e);
-		} catch (SecurityException e) {
-		  log.error(e.getMessage(), e);
-			throw new ReservationException(
-				"Security exception while creating local File named : " + pathToFile, e);
-		}
-		return size;
-	}
+    java.io.File localFile = new java.io.File(pathToFile);
+    try {
+      localFile.createNewFile();
+    } catch (IOException e) {
+      log.error(e.getMessage(), e);
+      throw new ReservationException("IO exception while creating local File named : " + pathToFile,
+          e);
+    } catch (SecurityException e) {
+      log.error(e.getMessage(), e);
+      throw new ReservationException(
+          "Security exception while creating local File named : " + pathToFile, e);
+    }
+    return size;
+  }
 
-	/**
-	 * Method that follows the contract specified in SpaceSystem Interface: please
-	 * refer there for further info.
-	 */
-	public long compactSpace(String pathToFile) throws ReservationException {
+  /**
+   * Method that follows the contract specified in SpaceSystem Interface: please refer there for
+   * further info.
+   */
+  public long compactSpace(String pathToFile) throws ReservationException {
 
-		String explanation = "Compact Space operation currently not supported!";
-		log.error(explanation);
-		throw new ReservationException(explanation);
-	}
+    String explanation = "Compact Space operation currently not supported!";
+    log.error(explanation);
+    throw new ReservationException(explanation);
+  }
 
-	/**
-	 * Method that follows the contract specified in SpaceSystem Interface: please
-	 * refer there for further info.
-	 */
-	public void removeSpace(String pathToFile) throws ReservationException {
+  /**
+   * Method that follows the contract specified in SpaceSystem Interface: please refer there for
+   * further info.
+   */
+  public void removeSpace(String pathToFile) throws ReservationException {
 
-		String explanation = "Remove Space operation currently not supported!";
-		log.error(explanation);
-		throw new ReservationException(explanation);
-	}
+    String explanation = "Remove Space operation currently not supported!";
+    log.error(explanation);
+    throw new ReservationException(explanation);
+  }
 
-	/**
-	 * Method that follows the contract specified in SpaceSystem Interface: please
-	 * refer there for further info.
-	 */
-	public long changeSize(String pathToFile, long newSize)
-		throws ReservationException {
+  /**
+   * Method that follows the contract specified in SpaceSystem Interface: please refer there for
+   * further info.
+   */
+  public long changeSize(String pathToFile, long newSize) throws ReservationException {
 
-		String explanation = "Change Size operation currently not supported!";
-		log.error(explanation);
-		throw new ReservationException(explanation);
-	}
+    String explanation = "Change Size operation currently not supported!";
+    log.error(explanation);
+    throw new ReservationException(explanation);
+  }
 
-	@Override
-	public String toString() {
+  @Override
+  public String toString() {
 
-		return "MockSpaceSystem";
-	}
+    return "MockSpaceSystem";
+  }
 }
